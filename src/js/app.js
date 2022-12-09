cbf3df39814844c0ddaff2a58fceaa08d8e485d1
Valentin Vladimirov <valentinvladimirov@abv.bg>
2022-12-09 15:10:41 +0200

    task-85 done

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,32 @@
 import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
-  // This block will be executed once the page is loaded and ready
+    // This block will be executed once the page is loaded and ready
+    let url = "https://pokeapi.co/api/v2/pokemon/?limit=10";
 
-  const ul = document.querySelector("ul");
+    const ul = document.querySelector("ul");
+
+    // fetch(url).then((response) => {
+    //     response.json().then((responseData) => {
+    //         for (let i = 0; i < responseData.results.length; i++) {
+    //             let li = document.createElement("li");
+
+    //             li.innerText = responseData.results[i].name;
+    //             ul.appendChild(li);
+    //         }
+    //     });
+    // });
+
+    fetch(url)
+        .then((response) => {
+            return response.json();
+        })
+        .then((responseData) => {
+            for (let i = 0; i < responseData.results.length; i++) {
+                let li = document.createElement("li");
+
+                li.innerText = responseData.results[i].name;
+                ul.appendChild(li);
+            }
+        });
 });
